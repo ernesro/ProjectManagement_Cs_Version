58f4715b529928fcde24da261776433726b9650d
Ernestas Urbonas <ernesrourbonas@gmail.com>
2023-05-02 17:31:46 +0200

    Login window

--- Project_Management/01view/UserWindowView.Designer.cs
+++ Project_Management/01view/UserWindowView.Designer.cs
@@ -28,12 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "UserWindowView";
+            loginBt = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            SuspendLayout();
+            // 
+            // loginBt
+            // 
+            loginBt.Location = new Point(66, 365);
+            loginBt.Name = "loginBt";
+            loginBt.Size = new Size(334, 37);
+            loginBt.TabIndex = 0;
+            loginBt.Text = "Login";
+            loginBt.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(66, 235);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(334, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(66, 117);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(334, 27);
+            textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(66, 77);
+            label1.Name = "label1";
+            label1.Size = new Size(53, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Email: ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(66, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(73, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Password:";
+            // 
+            // UserWindowView
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(465, 472);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(loginBt);
+            Name = "UserWindowView";
+            Text = "UserWindowView";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button loginBt;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label1;
+        private Label label2;
     }
 }
--- Project_Management/01view/UserWindowView.cs
+++ Project_Management/01view/UserWindowView.cs
@@ -16,5 +16,6 @@ namespace Project_Management._01view
         {
             InitializeComponent();
         }
+
     }
 }
